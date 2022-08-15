03133e34ecb00c9060efbe18e9275b2cc932517d
Silver Rooney <silver.rooney.dev@gmail.com>
2022-08-15 13:19:09 -0700

    Initial Commit

---
+++ Project15Accessibility/Project15AccessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project15AccessibilityApp.swift
+//  Project15Accessibility
+//
+//  Created by Silver on 8/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Project15AccessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
